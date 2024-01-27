7eac04f6e73cece9caa77d930b419eefd4dc05df
MuhammadYusuf0I <abduvasiyevmuhammadyusuf28@gmail.com>
2024-01-27 22:49:06 +0500

    homework

---
+++ index.js
@@ -0,0 +1,128 @@
+// 1-masala
+
+// function hajm(agr) {
+//     return agr**3
+// }
+
+// console.log(hajm(2));
+
+
+// 3-masala
+
+// function add(mas=[3,5,2,6]) {
+// return mas[0]
+// }
+
+// console.log(add());
+
+// 4-masal
+
+// function summed(num) {
+//     let sum=0;
+//  sum= Math.trunc(num/10);
+// return sum+=sum;
+// }
+
+// console.log(summed(123));
+
+// 5-masal
+
+// function year(year1) {
+//     if(year1%4==0&&year1%100!=0){
+//         return `Kasaba yili`
+//     } else {
+//         return `Kasaba yili emas`
+//     }
+// }
+
+// console.log(year(2000));
+
+// 6-masal
+
+// function randomNumber(min, max) {
+//     return Math.floor(Math.random() * (max - min) ) + min;
+//   }
+
+//   console.log(randomNumber(12,23));
+
+
+//  8-masala
+
+// function selsiy(farengeyt) {
+//     let max = Math.trunc(farengeyt-32) / 1.8;
+//     return max
+// }
+// console.log(selsiy(234));
+
+// 9-masala
+
+// function triangle(a, b, c) {
+//   let res = `Teng yonlik va Masshtabli uchburchak emas `;
+//   if (a + b == b + c) {
+//     res = `Teng yonli uchburchak`;
+//   } else if (a + b > b + c && b + c > a + c && a + c < a + b) {
+//     res = `Masshtabli uchburchak`;
+//   } else {
+//     res = `Teng yonlik va Masshtabli uchburchak emas `;
+//   }
+//   return res;
+// }
+
+// console.log(triangle(4, 5, 3));
+
+// 10-masala
+
+// function min(a,b) {
+//     if (a>b){
+//         return b
+//     }
+//     else{
+//         return a
+//     }
+// }
+// console.log(min(4,6));
+
+// 11-masala
+
+// function circle(radius) {
+//     return 3.14*radius
+// }
+// console.log(circle(9))
+
+// 13-masala
+
+// function cylinder(radius,height) {
+//     return (radius**2)*height
+// }
+// console.log(cylinder(4,6));
+
+// 16- Uchburchakning tomonlari uzunliklari berilgan perimetrini hisoblaydigan funksiyani ishlab chiqing.
+
+// function triangle(a,b,c) {
+//     return a+b+c
+// }
+// console.log(triangle(2,3,4));
+
+// 18-masala
+
+
+
+// function randomNumber(min, max) {
+//   let res = `Tasodifiy toq son emas`;
+//   let sum;
+//   sum = Math.floor(Math.random() * (max - min)) + min;
+//   if (sum%2 == 1) {
+//     res = `${sum} Tasodifiy toq son`;
+//   }
+//   return res
+// }
+
+// console.log(randomNumber(15, 73));
+
+
+// 19- Konusning radiusi va balandligini hisobga olgan holda uning hajmini hisoblaydigan funktsiyani tuzing.
+
+// function cone(radius,height) {
+//     return ((1/3)*(radius**2))*height
+// }
+// console.log(cone(4,9));
